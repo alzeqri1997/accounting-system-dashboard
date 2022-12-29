a615b38b32d1542f4015db0e41dd3935ce5690d0
Ahmed Al-zeqri <64102115+alzeqri1997@users.noreply.github.com>
2022-12-29 10:33:22 +0300

    install and setup i18n

---
+++ src/i18n.js
@@ -0,0 +1,12 @@
+import { createI18n } from "vue-i18n";
+import messages from "@intlify/unplugin-vue-i18n/messages"
+
+
+export default createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: 'ar',
+  fallbackLocale: 'en',
+  availableLocales: ["en", "de"],
+  messages:messages
+})
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueI18nnPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { dirname, resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(),
+    VueI18nnPlugin({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
+  }),
+    vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

ed61a1a6873fa55b4dcc0eb3f9105761e113a008
Ahmed Al-zeqri <64102115+alzeqri1997@users.noreply.github.com>
2022-12-30 18:45:02 +0300

    create InputField Component

---
+++ src/components/InputField.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineEmits(['input'])
+const input = defineProps({
+  type: {
+    type: String,
+    default: 'text'
+  },
+  label: {
+    type: String,
+  },
+  placeholder: {
+    type: String,
+    default: '------'
+  },
+  id: {
+    type: String,
+  },
+  value:{}
+})
+
+</script>
+
+<template>
+  <div class="tw-flex-grow" >
+    <label class="tw-block tw-text-gray tw-mb-[8px] "
+      for="name"> {{ label }}
+    </label>
+    <input class=" tw-w-full shadow1
+       tw-pt-[7px] tw-pb-[11px] tw-pr-[10px] tw-bg-primary-dark 
+       tw-rounded-[5px] tw-text-white tw-outline-none tw-border 
+       tw-border-[#363537] focus:tw-border-primary "
+      :id="id"
+      :type="type"
+      :placeholder="placeholder"
+      :value="value"
+      @input="$emit('input',$event.target.value)"
+      >
+  </div>
+</template>

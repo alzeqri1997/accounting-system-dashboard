6637a6a0e58c4ec1175f0e058ea8e20f401921f9
Ahmed Al-zeqri <64102115+alzeqri1997@users.noreply.github.com>
2022-12-30 18:48:53 +0300

    add routers

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,17 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      redirect:'services',
       name: 'home',
       component: HomeView
     },
     {
-      path: '/service',
+      path: '/services',
       name: 'service',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/Services.vue')
     },
     {
       path: '/about',

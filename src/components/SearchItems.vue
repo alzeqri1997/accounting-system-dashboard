0f3b4dda62bbe41389f3dccd60e1acddce092705
Ahmed Al-zeqri <64102115+alzeqri1997@users.noreply.github.com>
2022-12-30 18:46:31 +0300

    create SearchItems Component

---
+++ src/components/SearchItems.vue
@@ -0,0 +1,45 @@
+
+<script setup >
+import { ref, onUpdated } from 'vue';
+import InputField from './InputField.vue';
+const searchItems = ref({
+  name: "ahmed",
+  price:'',
+})
+
+onUpdated(() => {
+  // console.log(searchItems.value.name)
+})
+</script>
+
+<template>
+  <div class="bg-modal-linear tw-mx-[14px] tw-rounded-[10px] tw-py-[18px] tw-px-[25px]">
+    <h1 class="h1 tw-font-semibold tw-mb-[11px]"> البحث في العناصر </h1>
+    <div class="tw-flex tw-gap-[25px]">
+      <InputField type="text"
+        label="الاسم"
+        id="name"
+        :value="searchItems.name"
+        @input="(value)=> searchItems.name = value"
+        />
+      <InputField type="text"
+        label="السعر"
+        id="price"
+        :value="searchItems.price"
+        @input="(value)=> searchItems.price = value"
+        />
+      <InputField type="text"
+        label="السعر"
+        id="price"
+        :value="searchItems.price"
+        @input="(value)=> searchItems.price = value"
+        />
+        <InputField type="text"
+        label="الاسم"
+        id="name"
+        :value="searchItems.name"
+        @input="(value)=> searchItems.name = value"
+        />
+    </div>
+  </div>
+</template>

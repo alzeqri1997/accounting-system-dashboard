8a6500b99a03a585d6a462bfae8150c8e0661999
Ahmed Al-zeqri <64102115+alzeqri1997@users.noreply.github.com>
2022-12-31 08:23:02 +0300

    add Service Page

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,8 +1,10 @@
 <script setup>
 import SearchItems from '../components/SearchItems.vue';
+import ServiceSection from '../components/ServiceSection.vue'
 
 </script>
 
 <template>
   <SearchItems/>
+  <ServiceSection/>
 </template>

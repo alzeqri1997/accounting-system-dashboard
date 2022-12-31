1cdd8422238f20c5d836e79da04232b2e3a2972d
Ahmed Al-zeqri <64102115+alzeqri1997@users.noreply.github.com>
2022-12-31 17:41:19 +0300

    update Table Component

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,54 +1,63 @@
 <script setup >
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUpdated } from 'vue';
 import Arrow3 from './icons/Arrow3.vue'
+import TrashIcon from './icons/TrashIcon.vue'
+import EditIcon from './icons/EditIcon.vue'
+import Pagination from './Pagination.vue'
 // Reactive Data
 const TrDisplay = ref('auto')
 const tableContainer = ref();
 // Data
 const labels = [
-  { id: 0, name: '#' },
-  { id: 1, name: 'نوع الاشتراك' },
-  { id: 2, name: 'المتبقي' },
-  { id: 3, name: 'السعر' },
-  { id: 4, name: 'سعر البيع' },
-  { id: 5, name: 'المدة' },
-  { id: 6, name: 'الاجراءات' },
+  { id: 0, label: '#', name:'' },
+  { id: 1, label: 'نوع الاشتراك', name: 'subscriptionType' },
+  { id: 2, label: 'المتبقي', name: 'remaining' },
+  { id: 3, label: 'السعر', name: 'price'},
+  { id: 4, label: 'سعر البيع', name: 'seePrice' },
+  { id: 5, label: 'المدة', name: 'period' },
+  { id: 6, label: 'الاجراءات', name: ""},
 ]
 
 const data = [
   {
     id: 1,
-    subscriptionType: { color: '#ffde00', name: 'ggc' },
-    remaining: '200',
-    price: '6.3',
-    sellPrice: '16', // سعر البيع
-    period: '30'
+    subscriptionType: { color: '#ffde00', name: 'aaa' },
+    remaining: 200,
+    price: 6.3,
+    sellPrice: 16, // سعر البيع
+    period: 30
   },
   {
     id: 2,
-    subscriptionType: { color: '#6C6CFF', name: 'ggc' },
-    remaining: '200',
-    price: '6.3',
-    sellPrice: '16', // سعر البيع
-    period: '30'
+    subscriptionType: { color: '#6C6CFF', name: 'bbb' },
+    remaining: 50,
+    price: 7,
+    sellPrice: 17, // سعر البيع
+    period: 3
   },
   {
     id: 3,
-    subscriptionType: { color: '#F178B6', name: 'ggc' },
-    remaining: '200',
-    price: '6.3',
-    sellPrice: '16', // سعر البيع
-    period: '30'
+    subscriptionType: { color: '#F178B6', name: 'ccc' },
+    remaining: -20,
+    price: 7.3,
+    sellPrice: 18, // سعر البيع
+    period: 50
   },
   {
     id: 4,
-    subscriptionType: { color: '#fff', name: 'ggc' },
-    remaining: '200',
-    price: '6.3',
-    sellPrice: '16', // سعر البيع
-    period: '30'
+    subscriptionType: { color: '#fff', name: 'ddd' },
+    remaining: -50,
+    price: 8,
+    sellPrice: 19, // سعر البيع
+    period: 10
   },
 ]
+
+// Reactive Data
+const reactiveData = ref(data)
+const sortDirection = ref(1);
+
+
 
 // Functions 
 function sortBy(columnName) {
@@ -74,55 +83,69 @@ onMounted(() => {
 
 <template>
   <div class=" grow bg-modal-linear my-[15px] mx-[14px] rounded-[10px] py-[18px] px-[25px]">
-    <div ref="tableContainer" className="overflow-x-auto h-full">
-      <table className="table w-full h-full bg-primary-dark ">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th v-for="(label, index) in labels"
-              :key="index">
-              <div class="flex justify-between items-center" >
-                <span class="text-[20px] font-semibold ">{{ label.name }}</span>
-                <button>
-                  <Arrow3 />
-                </button>
-              </div>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- row 1 -->
-          <tr v-for="(item, index) in data"
-            :key="item.id">
-            <td>{{ index + 1 }}</td>
-            <td>
-              <div class="flex items-center " >
-                <span class="w-[19px] h-[19px] rounded-[5px] " :class="'bg-[' + item.subscriptionType.color + ']'" > </span>
-                <span>{{ item.subscriptionType.name }}</span>
-              </div>
-            </td>
-            <td>{{ item.remaining }}</td>
-            <td>{{ item.price }}</td>
-            <td>{{ item.sellPrice }}</td>
-            <td>{{ item.period }}</td>
-            <td>
-              <div>
-                <span>ahmed</span>
-                <span>mohammed</span>
-              </div>
-            </td>
-          </tr>
-          <tr :style="{height: '100%', 'display': TrDisplay}" >
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="flex flex-col gap-[25px] h-full">
+      <div ref="tableContainer"
+        className="overflow-x-auto h-full">
+        <table className="table w-full h-full bg-primary-dark">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th v-for="(label, index) in labels"
+                :key="index">
+                <div class="flex justify-between items-center">
+                  <span class="text-[20px] font-semibold ">{{ label.label }}</span>
+                  <button @click="sortBy(label.name)" class="sort">
+                    <Arrow3 />
+                  </button>
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- rows -->
+            <tr v-for="item in reactiveData"
+              :key="item.id">
+              <td>{{ item.id }}</td>
+              <td>
+                <div class="flex items-end ">
+                  <span :style="{ 'background-color': item.subscriptionType.color }"
+                    class="w-[19px] h-[19px] rounded-[5px] "> </span>
+                  <span class="pr-[11px]">{{ item.subscriptionType.name }}</span>
+                </div>
+              </td>
+              <td>
+                <div :class="{ 'bg-success': item.remaining >= 0, 'bg-error': item.remaining < 0 }"
+                  class="max-w-[133px] h-[40px] rounded-full center-flex">
+                  {{ item.remaining }}
+                </div>
+              </td>
+              <td>{{ item.price }}</td>
+              <td>{{ item.sellPrice }}</td>
+              <td>{{ item.period }}</td>
+              <td>
+                <div class="flex gap-[10px]">
+                  <button class="bg-icon3">
+                    <EditIcon />
+                  </button>
+                  <button @click="deleteItem(item)" class="bg-icon3">
+                    <TrashIcon />
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr :style="{ height: '100%', 'display': TrDisplay }">
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <Pagination/>
     </div>
   </div>
 </template>
@@ -131,22 +154,31 @@ onMounted(() => {
 /* rtl:begin:ignore */
 .table {
 
-  @apply rounded-[10px] ;
+  @apply rounded-[10px];
+
   th {
     @apply bg-primary-dark border-t-0 border border-secondary px-[26px] py-[24px] #{!important};
 
     &:first-child {
       @apply border-r-0 #{!important};
+
+      .sort {
+        display: none;
+      }
     }
 
     &:last-child {
       @apply border-l-0 #{!important};
+
+      .sort {
+        display: none
+      }
     }
   }
 
   tr {
     td {
-      @apply bg-primary-dark border-b-0 border border-secondary px-[26px] py-[24px] #{!important};
+      @apply bg-primary-dark border-b-0 border border-secondary px-[26px] py-[17px] #{!important};
 
       &:first-child {
         @apply border-r-0 #{!important};
@@ -159,8 +191,8 @@ onMounted(() => {
 
     // last child of td tag
     &:last-child {
-        @apply border-b-0 #{!important};
-      }
+      @apply border-b-0 #{!important};
+    }
 
   }
 }

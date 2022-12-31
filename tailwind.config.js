04243dd491369b3d687015cb547245e89d673c89
Ahmed Al-zeqri <64102115+alzeqri1997@users.noreply.github.com>
2022-12-31 13:28:57 +0300

    include daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   mode: "jit",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
+  plugins: [require("daisyui")],
+  extend: {},
   theme: {
     colors: {
       primary: "#6C6CFF",
@@ -10,31 +12,29 @@ module.exports = {
       white: "#FFFFFF",
       gray: "#888888",
     },
-    extend: {},
-    plugins: [require("daisyui")],
-    daisyui: {
-      styled: true,
-      themes: true,
-      base: false,
-      utils: true,
-      logs: true,
-      rtl: true,
-      darkTheme: "dark",
-      themes: [
-        {
-          mytheme: {
-            primary: "#6c6cff",
-            secondary: "#434348",
-            accent: "#252526",
-            neutral: "#191D24",
-            "base-100": "#2A303C",
-            info: "#3ABFF8",
-            success: "#27AE60",
-            warning: "#FBBD23",
-            error: "#EB5757",
-          },
+  },
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: false,
+    utils: true,
+    logs: true,
+    rtl: true,
+    darkTheme: "dark",
+    themes: [
+      {
+        mytheme: {
+          primary: "#6c6cff",
+          secondary: "#434348",
+          accent: "#252526",
+          neutral: "#191D24",
+          "base-100": "#2A303C",
+          info: "#3ABFF8",
+          success: "#27AE60",
+          warning: "#FBBD23",
+          error: "#EB5757",
         },
-      ],
-    },
+      },
+    ],
   },
 };
